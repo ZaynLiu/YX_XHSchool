205fb47876964156e94dcfa38390f1e93863ba92
刘祯晖Zayn <ynyx@ynyxdeiMac.local>
2016-07-29 14:05:41 +0800

    Create new project

---
+++ YX_XHSchool/YX_XHSchool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YX_XHSchool
+//
+//  Created by ynyx on 16/7/29.
+//  Copyright © 2016年 ynyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YX_XHSchool/YX_XHSchoolTests/YX_XHSchoolTests.m
@@ -0,0 +1,39 @@
+//
+//  YX_XHSchoolTests.m
+//  YX_XHSchoolTests
+//
+//  Created by ynyx on 16/7/29.
+//  Copyright © 2016年 ynyx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YX_XHSchoolTests : XCTestCase
+
+@end
+
+@implementation YX_XHSchoolTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
