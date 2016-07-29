80b7891990284a1c8d0bde28124f065e94b2b4f0
刘祯晖Zayn <ynyx@ynyxdeiMac.local>
2016-07-29 14:13:39 +0800

    recreate

---
+++ XHSchool/XHSchool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHSchool
+//
+//  Created by ynyx on 16/7/29.
+//  Copyright © 2016年 ynyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
