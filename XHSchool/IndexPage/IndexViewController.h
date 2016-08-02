227484e48316b64bf8ee749846aa5cf606819431
刘祯晖Zayn <ynyx@ynyxdeiMac.local>
2016-08-02 14:01:13 +0800

    创建大首页文件

---
+++ XHSchool/IndexPage/IndexViewController.h
@@ -0,0 +1,13 @@
+//
+//  IndexViewController.h
+//  XHSchool
+//
+//  Created by ynyx on 16/8/2.
+//  Copyright © 2016年 ynyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IndexViewController : UIViewController
+
+@end
